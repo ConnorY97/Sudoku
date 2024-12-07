21a66970080f73ae5ab22702643c3c1bdcd07e65
Connor Young <c_young97@live.com>
2024-12-07 11:31:29 +1100

    Removing example test
    Signed-off-by: Connor Young <c_young97@live.com>

--- app/src/test/java/com/example/sudoku/ExampleUnitTest.kt
+++ app/src/test/java/com/example/sudoku/ExampleUnitTest.kt
@@ -14,17 +14,6 @@ import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
-}
 
 class SudokuTest {
 
